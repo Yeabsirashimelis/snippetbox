c7afda68558cc2696f5c785237a5641a55cee399
yeabsira <shimelisyeabsiragithub@gmail.com>
2025-10-04 05:02:01 -0700

    Adding configuration via CLI flags and environment variables, Working with a custom logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 /*
 path which ends with a trailing slash are "subtree patterns" which are used to catchall
 */
 func main() {
+
+	/*
+			 Define a new command-line flag with the name "addr", a default value of ":4000" and some short help
+			  text explaining what the flag controls. the value of the flag will be stored in the addr variable at runtime.
+
+		  flag.string() - this has a benefit of converting whatever value the user provides at runtime to a string type.
+		                   if the value can't be converted to a string then the application will log an error and exit
+
+		  go has a range of other functions including flag.Int(), flag.Bool(),... - they automatically convert the command-line flg to the appropriate type.
+	*/
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	/*
+		   Importantly, we use the flag.parse() function to parse the commad-line flag. this reads in the command-line flag value
+		    and assigns it to the addr variable. you need to call this **before** you use the addr variable otherwise it will always
+			contain the default value ":4000". if any errors are encountered during parsing the aplication will be terminated
+	*/
+
+	flag.Parse()
+
+	/*
+		    use the log.new() to create a logger for writting information messages. this takes three parameters: the destination to write logs to (os.Stdout), a string
+			prefix for message (INFO followed by a tab) and flags to indicate that additional inforamtion to include (local date and time)
+			  NOTE THAT: the flags are joined using the bitwise OR operator |.
+	*/
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	/*
+	   create a logger for writting error messages in the same way, but the stderr as the destination and use the Log.Lshortfile flag to include the relevant
+	    file name and line number
+	*/
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) // Llongfile if you want to include the full-path
+
 	//Use http.NewServeMux() function to initialize a new servermux, then
 	//  register handler functions and corresponding URL patterns with the servemux
 	mux := http.NewServeMux()
@@ -40,9 +75,17 @@ func main() {
 	  to listen on (in this case ":4000") and the servemux we just created
 	*/
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	/*
+	   the value returned from the flag.String() function is a pointer to the flag value, not the value itself. so we need
+	     to dereference the pointer
+	*/
+
+	// log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 
 	//if the http.ListenAndServe() returns an error we use the log.Fatal() function to log the error message and exit
-	log.Fatal(err)
+	// log.Fatal(err)
+
+	errorLog.Fatal(err)
 }
